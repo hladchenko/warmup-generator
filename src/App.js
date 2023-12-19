0dde057b793fb441ee79810c2361f8c320cffe1a
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-12-19 20:20:32 +0200

    Questions added

--- src/App.js
+++ src/App.js
@@ -1,148 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import gsap from "gsap";
-import {useEffect} from "react";
+import {juniorQuestions} from "./questions";
+import {useState} from "react";
 
 function App() {
-  /*--------------------
-Vars
---------------------*/
-  useEffect(() => {
+    const questionSet = juniorQuestions;
+    const [question, setQuestion] = useState(questionSet[0]);
+    const [buttonPressed, setButtonPressed] = useState(false);
+    const clickHandler = async () => {
+        await new Promise(resolve => changeQuestion())
+            .then(() => showAnimation());
+    };
 
-  const $menu = document.querySelector('.menu');
-  const $items = document.querySelectorAll('.menu--item');
-  let menuHeight = $menu.clientHeight;
-  let itemHeight = $items[0].clientHeight;
-  let wrapHeight = $items.length * itemHeight;
+    const changeQuestion = async () => {
+        let timeout = 30;
+        while (timeout < 2000) {
+            setTimeout(() => {
+                const num = Math.floor(Math.random() * questionSet.length);
+                setQuestion(questionSet[num]);
+            }, timeout);
+            timeout += 100;
+        }
+    };
 
-  let scrollSpeed = 0;
-  let oldScrollY = 0;
-  let scrollY = 0;
-  let y = 0;
+    const showAnimation = () => {
+        setButtonPressed(true);
+        setTimeout(() => {
+            setButtonPressed(false);
+        }, 2000)
+    };
 
-
-  /*--------------------
-  Lerp
-  --------------------*/
-  const lerp = (v0, v1, t) => {
-    return v0 * (1 - t) + v1 * t;
-  };
-
-
-  /*--------------------
-  Dispose
-  --------------------*/
-  const dispose = scroll => {
-    gsap.set($items, {
-      y: i => {
-        return i * itemHeight + scroll;
-      },
-      modifiers: {
-        y: y => {
-          const s = gsap.utils.wrap(-itemHeight, wrapHeight - itemHeight, parseInt(y));
-          return `${s}px`;
-        } } });
-
-
-  };
-  dispose(0);
-
-
-  /*--------------------
-  Wheel
-  --------------------*/
-  const handleMouseWheel = e => {
-    scrollY -= e.deltaY;
-  };
-
-
-  /*--------------------
-  Touch
-  --------------------*/
-  let touchStart = 0;
-  let touchY = 0;
-  let isDragging = false;
-  const handleTouchStart = e => {
-    touchStart = e.clientY || e.touches[0].clientY;
-    isDragging = true;
-    $menu.classList.add('is-dragging');
-  };
-  const handleTouchMove = e => {
-    if (!isDragging) return;
-    touchY = e.clientY || e.touches[0].clientY;
-    scrollY += (touchY - touchStart) * 2.5;
-    touchStart = touchY;
-  };
-  const handleTouchEnd = () => {
-    isDragging = false;
-    $menu.classList.remove('is-dragging');
-  };
-
-
-  /*--------------------
-  Listeners
-  --------------------*/
-  $menu.addEventListener('mousewheel', handleMouseWheel);
-
-  $menu.addEventListener('touchstart', handleTouchStart);
-  $menu.addEventListener('touchmove', handleTouchMove);
-  $menu.addEventListener('touchend', handleTouchEnd);
-
-  $menu.addEventListener('mousedown', handleTouchStart);
-  $menu.addEventListener('mousemove', handleTouchMove);
-  $menu.addEventListener('mouseleave', handleTouchEnd);
-  $menu.addEventListener('mouseup', handleTouchEnd);
-
-  $menu.addEventListener('selectstart', () => {return false;});
-
-
-  /*--------------------
-  Resize
-  --------------------*/
-  window.addEventListener('resize', () => {
-    menuHeight = $menu.clientHeight;
-    itemHeight = $items[0].clientHeight;
-    wrapHeight = $items.length * itemHeight;
-  });
-
-
-  /*--------------------
-  Render
-  --------------------*/
-  const render = () => {
-    requestAnimationFrame(render);
-    y = lerp(y, scrollY, .1);
-    dispose(y);
-
-    scrollSpeed = y - oldScrollY;
-    oldScrollY = y;
-
-    gsap.to($items, {
-      scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * .005,
-      rotate: scrollSpeed * 0.2 });
-
-  };
-  render();
-
-  }, []);
-
-  return (
-    <div className="App">
-      <div className="menu">
-        <ul className="menu--wrapper">
-          <li className="menu--item"><button href="#">Facebook</button></li>
-          <li className="menu--item"><button href="#">Netflix</button></li>
-          <li className="menu--item"><button href="#">Instagram</button></li>
-          <li className="menu--item"><button href="#">Google</button></li>
-          <li className="menu--item"><button href="#">Twitter</button></li>
-          <li className="menu--item"><button href="#">Amazon</button></li>
-          <li className="menu--item"><button href="#">Pinterest</button></li>
-          <li className="menu--item"><button href="#">Spotify</button></li>
-          <li className="menu--item"><button href="#">YouTube</button></li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="menu--item">
+                    <div className="question-container">
+                        <div className={buttonPressed ? "question-shake" : "question"}>{question}</div>
+                    </div>
+                    <button onClick={clickHandler} className="button">Ask</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
