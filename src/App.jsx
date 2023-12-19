4d23391250f026aa9b2f09091a9d68d240df9484
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-12-19 21:11:16 +0200

    Add dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
+import './Button.scss';
 import {juniorQuestions} from "./questions";
 import {useState} from "react";
+import Dropdown from "./Dropdown/Dropdown";
 
 function App() {
     const questionSet = juniorQuestions;
     const [question, setQuestion] = useState(questionSet[0]);
     const [buttonPressed, setButtonPressed] = useState(false);
+    const [animateButton, setAnimateButton] = useState(false);
     const clickHandler = async () => {
+        setAnimateButton(true);
+        setTimeout(() => {
+            setAnimateButton(false);
+        }, 700);
+
         await new Promise(resolve => changeQuestion())
             .then(() => showAnimation());
     };
@@ -32,11 +40,14 @@ function App() {
     return (
         <div className="App">
             <div className="container">
+                <Dropdown />
                 <div className="menu--item">
                     <div className="question-container">
                         <div className={buttonPressed ? "question-shake" : "question"}>{question}</div>
                     </div>
-                    <button onClick={clickHandler} className="button">Ask</button>
+                    <button onClick={clickHandler}
+                            className={animateButton ? "bubbly-button animate" : "bubbly-button"}>Ask
+                    </button>
                 </div>
             </div>
         </div>
